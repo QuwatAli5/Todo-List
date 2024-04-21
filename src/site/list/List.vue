455f42e2637d63a6e8760cb15c22ed1a6f1267d4
Quwat <Quwat@TimeAhead.co>
2024-04-21 20:04:04 +0500

    List Items Filter By Title

--- src/site/list/List.vue
+++ src/site/list/List.vue
@@ -1,8 +1,14 @@
 <template>
     <LayoutAuthenticated>
         <SectionMain>
-            <SectionTitleLineWithButton :icon="mdiAccount" title="Todo List" main />
+            <SectionTitleLineWithButton :icon="mdiAccount" title="Todo List Items" main />
             <CardBox :classType="'light'">
+                
+                <FormField :classType="'light'">
+                    <span></span>
+                    <FormControl placeholder="Search by Title" v-model="search" :classType="'light'" :icon="mdiMail" />
+                </FormField>
+
                 <div class="genHeight h-450">
                     <table class="min-w-full border border-collapse border-gray-200 table-auto">
                         <thead>
@@ -14,7 +20,7 @@
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
-                            <tr v-for="(item, index) in listItems" :key="index">
+                            <tr v-for="(item, index) in displayedItems" :key="index">
                                 <td class="px-6 py-4 text-sm whitespace-no-wrap text-slate-800">
                                     {{ item.id }}
                                 </td>
@@ -43,11 +49,13 @@
 </template>
 
 <script setup>
-import { onMounted, ref} from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import { mdiAccount } from '@mdi/js';
 import apiRepository from "@/composables/apiRepository";
 import CommonFunction from "@/composables/CommonFunction";
 import Swal from 'sweetalert2';
+import FormField from '@/components/FormField.vue';
+import FormControl from '@/components/FormControl.vue';
 import CardBox from '@/components/CardBox.vue';
 import Paginate from "@/components/Paginate.vue";
 import SectionMain from '@/components/SectionMain.vue';
@@ -59,13 +67,38 @@ const { getAllList, deleteList } = apiRepository();
 const { fireToaster, truncateDescription } = CommonFunction();
 
 // Data Variables
+const search = ref(null);
 const processing = ref(false);
 const listItems = ref([]);
+const displayedItems = ref([]);
 const nextPageUrl = ref(null);
+
+// Watchers
+watch(() => search.value, (newVal) => {
+  if (newVal) {
+    const regex = new RegExp(newVal, 'i');
+    displayedItems.value = listItems.value.filter((item) =>
+      regex.test(item.title)
+    );
+  } else {
+    displayedItems.value = listItems.value;
+  }
+});
+
+watch (() => listItems.value, (newArr) => {
+  if(newArr) {
+    displayedItems.value = listItems.value;
+  }
+}, {immediate: true});
 
 // Functions
 const getListItems = (url = null) => {
     getAllList(url).then((response) => {
+        if(response.status == 401) {
+            localStorage.removeItem('token');
+            window.location.reload();
+        }
+        
         const { data } = response;
         listItems.value.push(...data.items.data);
         nextPageUrl.value = data.items.next_page_url;
