1883293206964911e468bf7a89928cf88fb0a74e
Quwat <Quwat@TimeAhead.co>
2024-04-21 17:26:15 +0500

    route authorization setup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import { computed, ref } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import Welcome from '@/site/Welcome.vue';
 import Login from '@/site/Login.vue';
@@ -5,6 +6,9 @@ import Register from '@/site/Register.vue';
 import Create from '@/site/list/Create.vue';
 import List from '@/site/list/List.vue';
 import Show from '@/site/list/Show.vue';
+
+const token = ref(localStorage.getItem('token'));
+const Authenticated = computed(() => token.value !== null && token.value != '');
 
 const routes = [
   {
@@ -20,7 +24,7 @@ const routes = [
     meta: {
       title: 'List'
     },
-    path: "/list",
+    path: "/",
     name: "list.index",
     component: List,
     meta: { requiresAuth: true },
@@ -70,6 +74,20 @@ const router = createRouter({
     //
     return { top: 0 };
   },
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.meta.requiresAuth;
+  const authenticated = Authenticated.value;
+
+
+  if (!requiresAuth && authenticated && (to.name == 'login' || to.name == 'register' || to.name == 'welcome')) {
+    next({ name: 'list.index' });
+  }else if(requiresAuth && !authenticated && (to.name == 'list.index' || to.name == 'list.create' || to.name == 'list.show')){
+    next({name:'login'})
+  }else{
+    next();
+  }
 })
 
 export default router;
